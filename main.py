219c472e5479ddf2ce9e708a73db70a943d164a9
SamroodAli <samroodali@live.com>
2021-01-11 12:52:02 +0530

    Chore:import datetime and contacts data to dictionary using orient records

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas
+import datetime as dt
+import random
 
-data = pandas.read_csv("birthdays.csv").to_dict(orient="records")
-print(data)
+contacts = pandas.read_csv("birthdays.csv").to_dict(orient="records")
+
+now = dt.datetime.now()
+for contact in contacts:
+    print(contact)
+    if contact["month"] == now.month and contact["day"] == now.day:
+       print("contact access correct")
